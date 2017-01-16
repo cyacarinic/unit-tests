f7efbc066b3da9ae7fcaa541f484b7b50bc61366
Claudio Yacarini <cyacarini@rcp.pe>
2017-01-16 10:58:26 -0500

    First commit

--- tests/ggwp.py
+++ tests/ggwp.py
@@ -7,11 +7,9 @@ class TestSequenceFunctions(unittest.TestCase):
         self.seq = range(10)
 
     def test_shuffle(self):
-        # make sure the shuffled sequence does not lose any elements
         random.shuffle(self.seq)
         self.seq.sort()
         self.assertEqual(self.seq, range(10))
-        # should raise an exception for an immutable sequence
         self.assertRaises(TypeError, random.shuffle, (1,2,3))
 
     def test_choice(self):
